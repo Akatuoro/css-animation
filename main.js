45e141b4535e9c67e83b85ffe18cbd2ef539250d
Johannes Vogt <akatuoro6@gmail.com>
2022-03-05 13:34:04 +0100

    use relative paths

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('sw.js', {scope: '/'}).catch(e => console.log(e));
+	navigator.serviceWorker.register('sw.js', {scope: ''}).catch(e => console.log(e));
 }
 
 let promptEvent;
